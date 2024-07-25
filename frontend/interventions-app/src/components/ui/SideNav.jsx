9c8b9b6b5d5f92bc7bd12eb4a2aa29bcf2ffa9d6
vynejoestar <vynejoestar@gmail.com>
2024-07-25 22:37:34 +0100

    sidenav updates

--- frontend/interventions-app/src/components/ui/SideNav.jsx
+++ frontend/interventions-app/src/components/ui/SideNav.jsx
@@ -7,12 +7,12 @@ const SideNav = () => {
         {/* Brand Logo */}
         <a href="index3.html" className="brand-link">
           <img
-            src="dist/img/AdminLTELogo.png"
+            src="/logo.png"
             alt="AdminLTE Logo"
-            className="brand-image img-circle elevation-3"
+            className="brand-image"
             style={{ opacity: ".8" }}
           />
-          <span className="brand-text font-weight-light">AdminLTE 3</span>
+          <span className="brand-text font-weight-light">Leyacom Solutions</span>
         </a>
         {/* Sidebar */}
         <div className="sidebar">
